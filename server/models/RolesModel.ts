e8e6d35f801b4cb6f79c13d58c02a10e4f1b61fa
rebecavm28 <rebe14@hotmail.es>
2024-03-26 14:49:22 +0100

    port

---
+++ server/models/RolesModel.ts
@@ -0,0 +1,10 @@
+import { Role } from "./RolesModel";
+
+// Crear un nuevo objeto de tipo Role
+const newRole: Role = {
+  id_rol: 1,
+  name_rol: 'Admin'
+};
+
+// Imprimir el nombre del rol
+console.log(newRole.name_rol);
