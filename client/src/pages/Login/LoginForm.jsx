bb64102786d02da1b505aecabcbe9e44493c1bcd
Andreamartinn17 <andreamartinn17@gmail.com>
2024-04-02 11:06:01 +0200

    first login form changes

--- client/src/pages/Login/LoginForm.jsx
+++ client/src/pages/Login/LoginForm.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const FormContainer = style.div`
+    width: 300px;
+    margin: 0 auto; 
+    text-align: center;
+`;
+const LoginFormTitle = style.div`
+
+
+`
 
 const LoginForm = () => {
+
+// Estado para almacenar los valores del usuario y la contraseña
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+// Manejadores de cambios para los campos de usuario y contraseña
+const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+// Manejador para enviar el formulario
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+// Aquí puedes agregar la lógica para enviar los datos al servidor, etc.
+    console.log('Usuario:', username);
+    console.log('Contraseña:', password);
+  };
+
   return (
-    <div>LoginForm</div>
+    <div className="FormContainer">
+    <div className="LoginFormTitle">Login</div>
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label>User</label>
+            <input type="text" id="username" value={username} onChange={handleUsernameChange}></input>
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" id="password" value={password} onChange={handlePasswordChange}></input>
+        </div>
+        <button type="submit">Sign in</button>
+    </form>
+    </div>
   )
 }
 
