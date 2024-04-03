72c278fbd13403ae6487bb501d6fb7ed34a7c00b
TrianaSolerMartin <triana287@gmail.com>
2024-04-03 15:26:34 +0200

    postmand method added

--- server/routes/NewsRouter.ts
+++ server/routes/NewsRouter.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { deleteNews, getNews, createNews, updateNews, showOneNews } from '../controllers/NewsController';
-import { validateNews } from '../validators/newsValidators';
+// import { validateNews } from '../validators/newsValidators';
 /* import { isAuthenticated } from '../middleware/authtenticalMiddleware';
  */
 
@@ -9,7 +9,7 @@ const newsRouter =  express.Router();
 newsRouter.get('/news',/* isAuthenticated, */ getNews);
 newsRouter.delete('/news/:id',/* isAuthenticated, */ deleteNews);
 newsRouter.get('/news/:id',/* isAuthenticated, */ showOneNews);
-newsRouter.post( '/news',/* isAuthenticated, */ validateNews, createNews);
-newsRouter.put('/news/:id', /* isAuthenticated, */ validateNews, updateNews);
+newsRouter.post( '/news',/* isAuthenticated, */  createNews);
+newsRouter.put('/news/:id', /* isAuthenticated, */  updateNews);
 
 export default newsRouter;
--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,20 +1,22 @@
-import * as jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config';
+import * as jwt from 'jsonwebtoken'; // Add missing import statement
 
-export const  createToken = async (user:any) => {
-    const  token = jwt.sign(
+export const createToken = async (user: any) => {
+    const token = jwt.sign(
         {
             id: user.id,
             rol: user.rol
         },
-        JWT_SECRET ,{ expiresIn: "2h"})
-        return token;
+        JWT_SECRET || '', // Provide a default value for JWT_SECRET
+        { expiresIn: "2h" }
+    );
+    return token;
 }
 
-export const  verifyToken = async (tokenJwt:any)=>{
+export const verifyToken = async (tokenJwt: string) => {
     try {
-       return jwt.verify(tokenJwt, JWT_SECRET) 
+        return jwt.verify(tokenJwt, JWT_SECRET || ''); // Provide a default value for JWT_SECRET
     } catch (error) {
-       return null 
+        return null;
     }
 }
