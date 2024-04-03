86ece1b7ee1ad2d1ab6559b2c54780ed7a033364
Nho89 <noa1720@gmail.com>
2024-04-03 10:47:59 +0200

    start-middleware-rol

---
+++ server/middleware/rolesMiddleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import UserModel from "../models/UserModel";
+import { verifyToken } from "../utils/jwt";
+
+
+export const  rolAuthenticated = async (request: Request, response: Response, next: NextFunction)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
