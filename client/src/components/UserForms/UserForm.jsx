03f2cf012e4803d594cac7e3681abaea05dd4ae4
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-05 13:34:37 +0200

    social styles completed and adding the logos

--- client/src/components/UserForms/UserForm.jsx
+++ client/src/components/UserForms/UserForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './UserFrom.css'
+import instagram_logo from '../../assets/svg/instagramCream.svg'
+import linkedin_logo from '../../assets/svg/linkedinCream.svg'
 
 const UserForm = () => {
   return (
@@ -31,9 +33,15 @@ const UserForm = () => {
 
         <div className="social">
             <h2 className='social_tittle'>Siguenos en redes sociales</h2>
-            <a className='social_links' href="http://" target="_blank" rel="noopener noreferrer">Instagram/Bitpress</a>
-            <a className='social_links' href="http://" target="_blank" rel="noopener noreferrer">Linkedin/Bitpress</a>
-        </div>
+                <div className="instagram_box">
+                    <img className='social_logo' src={instagram_logo} alt="instagram logo" />
+                    <a className='social_links' href="http://" target="_blank" rel="noopener noreferrer">Instagram/Bitpress</a>
+                </div>
+                <div className="linkedin_box">
+                    <img className='social_logo' src={linkedin_logo} alt="linkedin logo" />
+                    <a className='social_links' href="http://" target="_blank" rel="noopener noreferrer">Linkedin/Bitpress</a>
+                </div>
+            </div>
     </div>
   )
 }
