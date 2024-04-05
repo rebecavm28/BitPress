0a859805a3570b7b020e3acb4e8a11df8ab995d7
Andreamartinn17 <andreamartinn17@gmail.com>
2024-04-05 12:29:29 +0200

    conected myworkbench

--- server/routes/NewsRouter.ts
+++ server/routes/NewsRouter.ts
@@ -1,14 +1,14 @@
 import * as express from 'express';
 import { deleteNews, getAllNews, createNews, updateNews, showOneNews } from '../controllers/NewsController';
-import { isAuthenticated } from '../middleware/authtenticalMiddleware';
-import {rolAuthenticated} from "../middleware/rolesMiddleware"
+/* import { isAuthenticated } from '../middleware/authtenticalMiddleware';
+import {rolAuthenticated} from "../middleware/rolesMiddleware" */
 
 const newsRouter =  express.Router();
 
-newsRouter.get('/news',isAuthenticated,rolAuthenticated(["user","admin"]), getAllNews);
-newsRouter.delete('/news/:id',isAuthenticated,rolAuthenticated(["admin"]), deleteNews);
-newsRouter.get('/news/:id',isAuthenticated,rolAuthenticated(["user","admin"]), showOneNews);
-newsRouter.post('/news',isAuthenticated,rolAuthenticated(["user","admin"]), createNews);
-newsRouter.put('/news/:id', isAuthenticated,rolAuthenticated(["admin"]), updateNews);
+newsRouter.get('/news',/* isAuthenticated,rolAuthenticated(["user","admin"]), */ getAllNews);
+newsRouter.delete('/news/:id',/* isAuthenticated,rolAuthenticated(["admin"]), */ deleteNews);
+newsRouter.get('/news/:id',/* isAuthenticated,rolAuthenticated(["user","admin"]), */ showOneNews);
+newsRouter.post('/news',/* isAuthenticated,rolAuthenticated(["user","admin"]), */ createNews);
+newsRouter.put('/news/:id', /* isAuthenticated,rolAuthenticated(["admin"]), */ updateNews);
 
 export default newsRouter;
