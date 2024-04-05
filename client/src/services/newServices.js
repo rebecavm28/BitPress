b88d586e4cc3e27ad5226fa340e21f3f66c569e0
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-05 14:06:21 +0200

    change the url in newServices and push the server from develop

--- client/src/services/newServices.js
+++ client/src/services/newServices.js
@@ -5,7 +5,7 @@ import axios from "axios";
 // url de la base de datos = 'http://localhost:5000/'
 
 
-export const url = 'http://localhost:3000';
+export const url = 'http://localhost:5000/';
 
 
 //GET
