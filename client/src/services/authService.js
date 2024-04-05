00090a88977d2913e7bc44626c68eddf3779135d
Nho89 <noa1720@gmail.com>
2024-04-05 12:39:22 +0200

    intent-authServices

---
+++ client/src/services/authService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { url } from './userServices';
+
+
+export const login = async (variableForm ) => {
+ try {
+    
+ } catch (error) {
+    console.error('Error de login:', error.message);
+    throw error;
+ }
+};
