beb894fe3f70e5c542f089de6748af3e19c86742
TrianaSolerMartin <triana287@gmail.com>
2024-04-05 08:12:03 +0200

    credencial validation added

--- server/validators/usersValidators.ts
+++ server/validators/usersValidators.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 //USERSMODEL
 
-// Definir el esquema 
+// Validad el schema del modelo de usuario
 export const UserSchema = z.object({
     id_user: z.number().int().positive(),
     username: z.string().min(3, 'El nombre de usuario debe tener al menos 3 caracteres. 😁'),
@@ -13,46 +13,35 @@ export const UserSchema = z.object({
     created_at: z.date(),
     updated_at: z.date()
 });
-
-// Tipo de datos para los usuarios
+// Tipo de datos.
 export type User = z.infer<typeof UserSchema>;
-
-// Función para validar un usuario
+// Función
 export function validateUser(data: unknown): User {
     try {
-        // Intentamos analizar los datos proporcionados
         const validUser = UserSchema.parse(data);
-        // Si pasa la validación, devolvemos el usuario válido
         return validUser;
     } catch (error) {
-        // Capturamos cualquier error de validación
         console.error('Error de validación:', (error as Error).message);
-        // Devolvemos un objeto vacío si la validación falla
         return {} as User;
     }
 }
 
 //USER CREDENTIALS
-// Definir el esquema de validación para las credenciales de usuario
+// Definir credenciales de usuario
 export const UserCredentialsSchema = z.object({
     email: z.string().email('El email proporcionado no es válido. 🥶'),
     password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres.')
 });
 
-// Tipo de datos para las credenciales de usuario
+// Tipo de datos
 export type UserCredentials = z.infer<typeof UserCredentialsSchema>;
-
-// Función para validar las credenciales de un usuario
+// Función 
 export function validateUserCredentials(data: unknown): UserCredentials {
     try {
-        // Intentamos analizar los datos proporcionados
         const validCredentials = UserCredentialsSchema.parse(data);
-        // Si pasa la validación, devolvemos las credenciales válidas
         return validCredentials;
     } catch (error) {
-        // Capturamos cualquier error de validación
         console.error('Error de validación:', (error as Error).message);
-        // Devolvemos un objeto vacío si la validación falla
         return {} as UserCredentials;
     }
 }
