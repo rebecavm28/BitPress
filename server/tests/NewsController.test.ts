20148722ad26c1f45c18cc1a6f36c8869ed3c9ab
rebecavm28 <rebe14@hotmail.es>
2024-04-08 12:26:55 +0200

    test

--- server/tests/NewsController.test.ts
+++ server/tests/NewsController.test.ts
@@ -1,17 +1,28 @@
 import request from 'supertest';
 import {app} from '../app';
+import NewsModel from '../models/NewsModel';
 
 
 
 const api = request(app);
 
 
-describe('Crear Noticia', () => {
 
-    test('debería devolver noticias', async () => {
-    const response = await request(app).get('/news');
-    console.log(response); // Imprime la respuesta para verificar su contenido
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('noticias');
-  });
-})
+//  describe('PUT', () => {
+//     let createdNews = {};
+//     beforeEach(async ()=> {
+//         createdNews = await NewsModel.create({
+//           titulo: 'Nueva Noticia',
+//           imageURL:'url',
+//           contenido: 'Contenido de la nueva noticia',
+//           date:'2012-03-01',
+//           id_user:1
+//         })
+//         test('debería devolver noticias', async () => {
+//           const response = await request(app).get('/news');
+//           console.log(response); // Imprime la respuesta para verificar su contenido
+//           expect(response.status).toBe(200);
+//           expect(response.body).toHaveProperty('noticias');
+
+//     })
+// })})
