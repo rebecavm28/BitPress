1e9760d328d63c3003e5126ffa079027c932d24c
Andreamartinn17 <andreamartinn17@gmail.com>
2024-04-09 09:12:16 +0200

    changes in feature-put

--- client/src/pages/AddForm/AddForm.jsx
+++ client/src/pages/AddForm/AddForm.jsx
@@ -1,32 +1,54 @@
 import React from 'react'
 import './AddForm.css'
+import { useForm } from "react-hook-form";
+//import { postNew } from 'client\src\services\newServices'
+import { useNavigate } from "react-router-dom";
 
 const AddForm = () => {
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    const navigate = useNavigate()
+    const onSubmit = async (Data) => {
+        await postNew(Data)
+        navigate("/")
+    };
+ 
     return (
         <div className='formAdd'>
             <div>
                 <h2>Add new news</h2> 
             </div>
-            <form> 
+            <form onSubmit={handleSubmit(onSubmit)}> 
                 <div className='formFields'>
-                    <label htmlFor="title"className='addTitle'>Add the title of the news</label>
-                    <input type="text" id="title" name="title"/>
+                    <label htmlFor="title"className='addTitle'>Add the title of the new</label>
+                    <input type="text" id="title" name="title" {...register('title', { 
+                        pattern: /^[a-zA-Z0-9áéíóúÁÉÍÓÚüÜñÑ\s\.,:;!¿¡?]+$/,
+                        required: true, maxLength:  100 
+                    })}/>
+                    {errors.title?.type === 'required' && <p className="error-message">Please, add the title of the new</p>}
                 </div>
                 <div className='formFields'>
                     <label htmlFor="image"className='image'>Imagen</label>
-                    <input type="url" id="image" name="image"/>
+                    <input type="url" id="image" name="image" {...register('image', { 
+                        pattern: /^(https?|ftp):\/\/[^\s\/$.?#].[^\s]*$/,
+                        required: true 
+                    })}/>
+                    {errors.image?.type === 'required' && <p className="error-message">Please, add the image of the new</p>}
                 </div>
                 <div className='formFields'>
                     <label htmlFor="description"className='addDescription'>Add the news description</label>
-                    <input type="text" id="description" name="description"/>
+                    <input type="text" id="description" name="description"{...register('description', { 
+                        pattern: /^[a-zA-Z0-9áéíóúÁÉÍÓÚüÜñÑ\s\.,:;!¿¡?]+$/,
+                        required: true, maxLength:  1000 
+                    })}/>
+                    {errors.description?.type === 'required' && <p className="error-message">Please, add the description of the new</p>}
                 </div>
-                <input className="buttonAdd" type="submit" value="Añadir noticia" />
+                <input className="buttonAdd" type="submit" value="ADD NEW"/>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default AddForm
+export default AddForm;
 
 
 
