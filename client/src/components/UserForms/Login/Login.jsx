bc695272c2527a93761373709034977574f49099
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-09 10:27:22 +0200

    making the folders to organizate the usersForm in diferent components

---
+++ client/src/components/UserForms/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <form className="login">
+        <input type="text" className="login_username" placeholder="Username" required/>
+        <input type="password" className="login_password" placeholder="Password" required/>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
