b9970c9220aeb978a34ee6355a04b1ab2238c96a
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-10 21:11:55 +0200

    add useNavigate to home in navbar logo

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import '../Navbar/Navbar.css'
 import logo from '../../assets/images/logo_negro.png'
 import texto from '../../assets/images/texto_logo.png'
-
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
   return (
     <nav>
 
-    <div className="logo_container">
+    <div className="logo_container" onClick={() => navigate(`/`)} >
       <img src={logo} alt="" className='image1'/>
       <img src={texto} alt="" className='image2'/>
     </div>
