3eba3caa1388fd0513b8e4cebd993795495ff49e
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-10 16:52:56 +0200

    delete carrousel in home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
       <div className='home_container'>
         <main>
-          <div className="news_carrousel"></div>
+          {/* <div className="news_carrousel"></div> */}
             <div className="slogan">
               <h3 className='tittle_1'>THE BEST</h3>
               <h1 className='tittle_2'>TECH NEWS</h1>
