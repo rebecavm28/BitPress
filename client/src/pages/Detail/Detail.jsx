95df5f9cad0ad7b26860a6be7cf8078b1da5240d
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-12 12:54:26 +0200

    change url in services, add handleDelete in details and delete navigate in img details

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,47 +1,60 @@
 import { useParams } from "react-router";
 import { useEffect, useState } from 'react';
-import { getNewById } from '../../services/newServices'
+import { getNewById, deleteData } from '../../services/newServices'; // Asumiendo que existe una función deleteData en tu servicio
 import './Detail.css';
 
 const Detail = () => {
   const { id_news } = useParams();
-    const [ data, setData ] = useState(null);
-  
+  const [ data, setData ] = useState(null);
 
-      
-      useEffect(() => {
-        const fetchData = async () =>{
-            const response = await getNewById(id_news); 
-             setData(response); 
-        }
-        fetchData()
-    }, [id_news]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getNewById(id_news); 
+        setData(response); 
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [id_news]);
 
+  const handleDelete = async () => {
+    try {
+      await deleteData(id_news);
 
-return (
-  <div>
-    {data ? (<div className="chargingBox">
-        <h2 className="titleNew">{data.tittle}</h2>
-        <img className="imgNew" src={data.imageUrl}/>
-      <div className="subtitles">
-        <p><span className="author">User : </span>{data.user}<span className="date"> | Date : </span>{data.date}</p>
-        <h3 className="subDescrip">DESCRIPTION:</h3>
-        <div className="description">
-          <p>{data.content}</p></div>
-        <div className='buttons'>
-          <button className="bEdit">EDIT</button>
-          <button className="bDelete">DELETE</button>
-          {/* <button onClick={handleClick}>Edit</button>
-          <button onClick={handleClick}>Delete</button> */}
+      const {token, rol} = response.sesiondata;
+      localStorage.setItem('token', token);
+      localStorage.setItem('rol', rol);
+          //añadir redireccion a la home
+    } catch (error) {
+          console.error(error)
+    }
+  };
+
+  return (
+    <div>
+      {data ? (
+        <div className="chargingBox">
+          <h2 className="titleNew">{data.title}</h2>
+          <img className="imgNew" src={data.imageUrl} alt="News Image" />
+          <div className="subtitles">
+            <p><span className="author">User: </span>{data.user}<span className="date"> | Date: </span>{data.date}</p>
+            <h3 className="subDescrip">DESCRIPTION:</h3>
+            <div className="description">
+              <p>{data.content}</p>
+            </div>
+            <div className="buttons">
+              <button className="bEdit">EDIT</button>
+              <button className="bDelete" onClick={handleDelete}>DELETE</button>
+            </div>
+          </div>
         </div>
-        </div>
-      </div>
-    ) : (
-    <p>Charging news...</p>
-    )}
-  </div>
-);
+      ) : (
+        <p>Charging news...</p>
+      )}
+    </div>
+  );
 };
-
 
 export default Detail;
