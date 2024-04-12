9cecfe1edada13e7c6e090e0a125dd573ec980d5
Nho89 <noa1720@gmail.com>
2024-04-12 14:24:35 +0200

    fail-helper-test

--- server/tests/helpers/helperTest.ts
+++ server/tests/helpers/helperTest.ts
@@ -1,9 +1,9 @@
 
-export userAdminTest = {
+/* export userAdminTest = {
     
         "name": "test",
         "email": "test@gmail.com",
         "password": "test",
         "rol": "admin"
     
-}
+} */
