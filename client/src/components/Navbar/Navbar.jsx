29ea69f1f5c7a7304bd8132cd55a6b5dbd6c4f0e
Nho89 <noa1720@gmail.com>
2024-04-15 21:36:35 +0200

    modified services frontend

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import '../Navbar/Navbar.css'
 import logo from '../../assets/images/logo_negro.png'
 import texto from '../../assets/images/texto_logo.png'
 import { useNavigate } from 'react-router-dom'
+import { useUserContext } from '../../context/UserContext';
+import LogoutButton from '../Logout-butoon/logout-button';
 
 const Navbar = () => {
+  const { isAuthenticated } = useUserContext();
 
   const navigate = useNavigate();
 
@@ -14,9 +17,16 @@ const Navbar = () => {
       <img src={texto} alt="" className='image2'/>
     </div>
     <ul className='list_box'>
-    <li><button className='navbar_link violet' onClick={() => navigate('/login')}>Login</button></li>
-        <li><button className='navbar_link cream' onClick={() => navigate('/register')}>Register</button></li>
-    </ul>
+        {isAuthenticated ? (
+          <LogoutButton />
+        ) : (
+          <>
+            <li><button className='navbar_link violet' onClick={() => navigate('/login')}>Login</button></li>
+            <li><button className='navbar_link cream' onClick={() => navigate('/register')}>Register</button></li>
+          </>
+        )}
+      </ul>
+
   </nav>
   )
 }
