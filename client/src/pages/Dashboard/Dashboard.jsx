c2d814ba39038348e1da78fe9a5cd744c29b3fcf
TrianaSolerMartin <triana287@gmail.com>
2024-04-15 15:40:05 +0200

    setUser and user:  fixed and added into Logoutcomponent

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,6 @@
 import './Dashboard.css'
 import NewsGallery from '../../components/Gallery/NewsGallery'
 import { useNavigate } from 'react-router-dom'
-import LogoutButton from '../../components/Logout-butoon/logout-button';
 
 const Dashboard = () => {
 
@@ -10,7 +9,6 @@ const Dashboard = () => {
   return (
       <div className='home_container'>
         <main>
-          <LogoutButton />
           <div className="circle_button">
             <h2 className='button_letters' onClick={() => navigate(`add`)} >AÑADIR NOTICIA</h2>
           </div>
