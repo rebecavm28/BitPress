cbbc7ecb40e2ec770832e7bbedc18d7efec17213
Nho89 <noa1720@gmail.com>
2024-04-16 15:25:54 +0200

    modified-css

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -8,8 +8,14 @@ const Dashboard = () => {
 
   return (
       <div className='home_container'>
-        <main>
-          <div className="circle_button">
+        <main className='contain_sup_dash'>
+          <div className="slogan_dash">
+              <h3 className='tittle_1_dash'>THE BEST</h3>
+              <h1 className='tittle_2_dash'>TECH NEWS</h1>
+              <div className="color_back_dash"></div>
+              <div className="blur_effect_dash"></div>
+            </div>
+           <div className="circle_button">
             <h2 className='button_letters' onClick={() => navigate(`add`)} >AÑADIR NOTICIA</h2>
           </div>
         </main>
