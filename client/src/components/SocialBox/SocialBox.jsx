60d976737cd899ef8c1993f2cd0959a954131c07
Andreamartinn17 <andreamartinn17@gmail.com>
2024-04-18 15:46:30 +0200

    add css in socialbox

--- client/src/components/SocialBox/SocialBox.jsx
+++ client/src/components/SocialBox/SocialBox.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import instagram_logo from '../../assets/svg/instagramCream.svg'
 import linkedin_logo from '../../assets/svg/linkedinCream.svg'
+import "./SocialBox.css"
 
 const SocialBox = () => {
   return (
       <div className="social_container">
               <h2 className='social_tittle'>Síguenos en redes sociales</h2>
+                <div className='socialstyles'>
                   <div className="instagram_box">
                       <img className='social_logo' src={instagram_logo} alt="instagram logo" />
                       <a className='social_links' href="http://" target="_blank" rel="noopener noreferrer">Instagram/Bitpress</a>
                   </div>
+                  
                   <div className="linkedin_box">
                       <img className='social_logo' src={linkedin_logo} alt="linkedin logo" />
                       <a className='social_links' href="http://" target="_blank" rel="noopener noreferrer">LinkedIn/Bitpress</a>
+                  </div>
                   </div>
       </div>
   )
